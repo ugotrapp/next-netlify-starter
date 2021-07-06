01bd25a51f8f44989b16653a7950c8a2c3a6d036
gitTrap <ugo.trapuzzano@gmail.com>
2021-07-06 17:05:32 +0200

    first commit

--- pages/index.js
+++ pages/index.js
@@ -6,9 +6,10 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Hugo.js Starter!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      
 
       <main>
         <Header title="Welcome to my app!" />
